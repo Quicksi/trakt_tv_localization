87c6b3c0b5ac728a488923e0c4e1721e407ed2de
Quicks <Quicksi@users.noreply.github.com>
2017-04-26 15:19:45 +0200

    some bugfixing and improvements
    oh i really wanted to write "fixes and improvements" for a log time now ;-P

--- trakt_tv_localization.quicks.user.js
+++ trakt_tv_localization.quicks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Trakt.tv localization
 // @namespace https://github.com/Quicksi/trakt_tv_localization
-// @version 0.2
+// @version 0.3
 // @description Translates stuff on trakt.tv to other languages
 // @match https://trakt.tv/movies/*
 // @copyright 2017+, Quicks
@@ -42,6 +42,105 @@ function getTMDBDataByNameYear(title, year){
 
 
 // TRANSLATOR
+function transMovieOverview() {
+    // Get all movies
+    tl_movies = $('.grid-item');
+
+    // check last title
+    if ( ( $(tl_movies[0]).find('.titles').children('h3')[0].childNodes[0].data != lastTitle ) &&
+        ( $(tl_movies[0]).find('.titles').find('h5').length === 0) )
+    { // last Title changed
+        // set new lastTitle
+        lastTitle = $(tl_movies[0]).find('.titles').children('h3')[0].childNodes[0].data;
+
+        // Translate now!
+        //console.log('translate of all');
+
+        sem = false;
+        tl_movies.each(function(index) {
+            qsself[index] = this;
+            // get Titles DIV
+            titlesDiv = $(this).find('.titles')[0];
+            // Get tile and year for the movie
+            title_org = $(titlesDiv).children('h3')[0].childNodes[0].data;
+            title_org = title_org.substr(0, title_org.length-1);
+            year_org = $(titlesDiv).children('h3')[0].childNodes[1].innerHTML;
+
+            // Get German Data
+            getTMDBDataByNameYear(title_org, year_org).done( function(promise) {
+                // check if title was found
+                if( promise.results.length > 0 ) {
+                    // Add h5 for org title
+                    divOrgTitle = document.createElement("h5");
+                    title_org = $(qsself[index]).find('.titles').children('h3')[0].firstChild.data;
+                    divOrgTitle.innerHTML = '(' + title_org.substr(0, title_org.length-1) + ')';
+                    $(qsself[index]).find('.titles')[0].append(divOrgTitle);
+
+                    // Translate title
+                    $(qsself[index]).find('.titles').children('h3')[0].firstChild.data = promise.results[0].title + ' ';
+                    if (index === 0) lastTitle =  $(qsself[index]).find('.titles').children('h3')[0].firstChild.data;
+
+                    // Add TMDB Rating
+                    rating_org = $(qsself[index]).find('.percentage')[0].innerHTML;
+                    $(qsself[index]).find('.percentage')[0].innerHTML = rating_org + ' / ' + promise.results[0].vote_average;
+                }
+                else {
+                    console.log('Localization not found for "' + $(qsself[index]).children('h3')[0].firstChild.data + '"');
+                    $(qsself[index]).find('.titles').children('h3')[0].firstChild.data += '(en)';
+                }
+            });
+        });
+        sem = true;
+    }
+}
+
+function transSingleMovie() {
+    elemID=document.getElementsByClassName("mobile-title");
+    // check last title
+    if ( elemID[0].firstChild.firstChild.data != lastTitle )
+    { // last Title changed
+        // set new lastTitle
+        lastTitle = elemID[0].firstChild.firstChild.data;
+
+        // Translate now!
+        //console.log('translate of one');
+
+        sem = false;
+        // find TMDB ID
+        var tmdbLink = $('a[href^="https://www.themoviedb.org/movie/"]');
+        var tmdb_id = tmdbLink[0].href.substring(33);
+
+        // create new element for original title
+        var newElement=document.createElement("span");
+
+        // Get German title from TMDB
+        getTMDBDataById(tmdb_id).done( function(promise) {
+            //console.log(promise);
+            // set newElement to Original title
+            newElement.innerHTML = 'Original title: ' + elemID[0].firstChild.firstChild.data;
+
+            // Set localized title
+            elemID[0].firstChild.firstChild.data = promise.title + ' ';
+            lastTitle = elemID[0].firstChild.firstChild.data;
+
+            // Add localized Plot
+            plotDiv = $('div[itemprop="description"]');
+            plot = $(plotDiv).html();
+            plot += '<br /><br />Localized:<br />' + promise.overview;
+            $(plotDiv).html(plot);
+
+            // Add TMDB Rating
+            var newLi=document.createElement("li");
+            newLi.innerHTML = '<div class="number"><div class="rating">' + promise.vote_average + '/' + promise.vote_count + '</div><div class="votes">TMDB</div></div>';
+            $(".ratings")[0].append(newLi);
+        });
+
+        // Add element to DOM
+        $(elemID).append(newElement);
+        sem = true;
+    }
+}
+
 function translateMe() {
     if ( tmdb_api_key === '' && isActive === true) {
         isActive = false;
@@ -49,87 +148,12 @@ function translateMe() {
     } else {
         // Try to find a "mobile-title"-class ... if found,we are on single movie page
         elemID=document.getElementsByClassName("mobile-title");
-        if ( elemID.length === 0 )
-        { // here we are on movies overview
-            // Get all movies
-            movies = $('.titles');
-
-            // check last title
-            if ($(movies[0]).children('h3')[0].childNodes[0].data != lastTitle )
-            { // last Title changed
-                // set new lastTitle
-                lastTitle = $(movies[0]).children('h3')[0].childNodes[0].data;
-
-                // Translate now!
-                //console.log('translate of all');
-
-                sem = false;
-                movies.each(function(index) {
-                    qsself[index] = this;
-                    // Get tile and year for the movie
-                    title_org = $(this).children('h3')[0].childNodes[0].data;
-                    title_org = title_org.substr(0, title_org.length-1);
-                    year_org = $(this).children('h3')[0].childNodes[1].innerHTML;
-
-                    // Get German Data
-                    getTMDBDataByNameYear(title_org, year_org).done( function(promise) {
-                        // check if title was found
-                        if( promise.results.length > 0 ) {
-                            $(qsself[index]).children('h3')[0].firstChild.data = promise.results[0].title + ' ';
-                            if (index === 0) lastTitle =  $(qsself[index]).children('h3')[0].firstChild.data;
-                        }
-                        else {
-                            console.log('Localization not found for "' + $(qsself[index]).children('h3')[0].firstChild.data + '"');
-                            $(qsself[index]).children('h3')[0].firstChild.data += '(en)';
-                        }
-                    });
-                });
-                sem = true;
-            }
-        }
-        else
-        { // here we are on single movie page
-            // check last title
-            if ( elemID[0].firstChild.firstChild.data != lastTitle )
-            { // last Title changed
-                // set new lastTitle
-                lastTitle = elemID[0].firstChild.firstChild.data;
-
-                // Translate now!
-                //console.log('translate of one');
-
-                sem = false;
-                // find TMDB ID
-                var tmdbLink = $('a[href^="https://www.themoviedb.org/movie/"]');
-                var tmdb_id = tmdbLink[0].href.substring(33);
-
-                // create new element for original title
-                var newElement=document.createElement("span");
-
-                // Get German title from TMDB
-                getTMDBDataById(tmdb_id).done( function(promise) {
-                    console.log(promise);
-                    // set newElement to Original title
-                    newElement.innerHTML = 'Original title: ' + elemID[0].firstChild.firstChild.data;
-                    elemID[0].firstChild.firstChild.data = promise.title + ' ';
-                    lastTitle = elemID[0].firstChild.firstChild.data;
-
-                    // Add German Plot
-                    plotDiv = $('div[itemprop="description"]');
-                    plot = $(plotDiv).html();
-                    plot += '<br /><br />Localized:<br />' + promise.overview;
-                    $(plotDiv).html(plot);
-
-                    // Add TMDB Rating
-                    var newLi=document.createElement("li");
-                    newLi.innerHTML = '<div class="number"><div class="rating">' + promise.vote_average + '/' + promise.vote_count + '</div><div class="votes">TMDB</div></div>';
-                    $(".ratings")[0].append(newLi);
-                });
-
-                // Add element to DOM
-                $(elemID).append(newElement);
-                sem = true;
-            }
+        if ( elemID.length === 0 ) {
+            // here we are on movies overview
+            transMovieOverview();
+        } else {
+            // here we are on single movie page
+            transSingleMovie();
         }
     }
 }
